3b737272cd2e999002c6abbf4cedce55ef137494
maykol68 <sm9365277@gmail.com>
2023-01-16 04:24:59 -0500

    primer commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,55 @@
+class ProductsController < ApplicationController
+
+    def index
+        @products = Product.all.with_attached_photo
+    end
+    
+    def show
+        product
+    end
+
+    def new
+        @product = Product.new 
+    end
+
+    def create
+        @product = Product.new(product_params)
+
+        if @product.save
+            redirect_to products_path, notice: t('.created') 
+        else
+            render :new, status: :unprocessable_entity 
+        
+       end
+       #para mostrar en consola
+        #pp @product
+    end
+
+    def edit
+        product
+    end
+
+    def update
+
+        if product.update(product_params)
+            redirect_to product_path, notice: t('.updated') 
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        product.destroy
+        redirect_to product_path, notice:  t('.destroyed') , status: :see_other
+    end
+
+    private
+    def product_params
+        params.require(:product).permit(:title, :description, :price, :photo)
+    end
+    
+    def product
+        @product = Product.find(params[:id])
+    end
+
+end
---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+    test 'render a list of products' do
+        get products_path
+
+        assert_response :success
+        assert_select '.product', 2
+    end
+
+    test 'render a detailed product page' do
+        get product_path(products(:ps4))
+
+        assert_response :success
+        assert_select '.title', 'ps4 pro'
+        assert_select '.description', 'ps4 en mal estado'
+        assert_select '.price', '150'
+    end
+
+    test 'render a new product form' do
+        get new_product_path
+
+        assert_response :success
+        assert_select 'form'
+    end
+
+    test 'allow to create a new product' do
+        post products_path, params: {
+            product: {
+                title: 'Nintendo 64',
+                description:'le faltan los cables',
+                price: 45
+            }
+        }
+        assert_redirected_to products_path
+    end
+
+    test 'does not allow to create a new product with empty fields' do
+        post products_path, params: {
+            product: {
+                title: '',
+                description:'le faltan los cables',
+                price: 45
+            }
+        }
+        assert_response :unprocessable_entity
+    end
+end
